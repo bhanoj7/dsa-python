32dc7e7d3816802c39a07a03d68b8f93a4be4dba
bhanoj <bhanojaddala792000@gmail.com>
2025-11-29 15:37:02 +0530

    Day 15 linkedList Cycle using Floyd's algorithm

---
+++ problems/linkedList/linked_list_cycle.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+
+
+if __name__ == "__main__":
+    # Create cycle: 3 -> 2 -> 0 -> -4 -> back to 2
+    head = ListNode(3)
+    head.next = ListNode(2)
+    head.next.next = ListNode(0)
+    head.next.next.next = ListNode(-4)
+    head.next.next.next.next = head.next  # create cycle
+
+    sol = Solution()
+    print(sol.hasCycle(head))  # True
